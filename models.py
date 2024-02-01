5d807eb6ee9ff4bf1f214753e52c3c0c13f9a171
xxz <14035544+xxz496090@user.noreply.gitee.com>
2024-02-01 22:11:16 +0800

    patientinfo

--- models.py
+++ models.py
@@ -1,19 +1,28 @@
+from datetime import datetime
 from exts import db
 
 
 class Doctor(db.Model):
     __tablename__ = 'doctor'
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.String(100), primary_key=True, nullable=False)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(300), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
 
-class Patient(db.Model):
-    __tablename__ = 'patient'
+class PatientInfo(db.Model):
+    __tablename__ = 'patientinfo'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(300), nullable=False)
+    mobile = db.Column(db.String(100), unique=True, nullable=False)
+    address = db.Column(db.String(200))
+    job = db.Column(db.Integer)
+    nation = db.Column(db.String(100))
+    marital_status = db.Column(db.Boolean)
+    emergencyContact = db.Column(db.String(100))
+    createDate = db.Column(db.DateTime, default=datetime.now)
+    updateDate = db.Column(db.DateTime, default=datetime.now)
 
 
 class Admin(db.Model):
