a4a67f09c70fc5322f4f2b0f0e69d3f9e79f8ad8
xxz <14035544+xxz496090@user.noreply.gitee.com>
2024-02-02 21:16:35 +0800

    增加注释

--- blueprint/doctor.py
+++ blueprint/doctor.py
@@ -8,20 +8,39 @@ bp = Blueprint('doctor', __name__, url_prefix='/')
 
 @bp.route('/new_patient', methods=['POST'])
 def new_patient():
+    """
+    创建新患者记录--text
+
+    Args:
+        无
+
+    Returns:
+        dict: 新创建的患者对象
+    """
+    # 从请求的JSON数据中获取参数
     f = request.json
+    # 获取id参数
     id = f.get('id')
+    # 获取username参数
     username = f.get('username')
+    # 获取password参数
     password = f.get('password')
+    # 获取mobile参数
     mobile = f.get('mobile')
+    # 获取address参数
     address = f.get('address')
+    # 获取job参数
     job = f.get('job')
+    # 获取nation参数
     nation = f.get('nation')
+    # 获取marital_status参数
     marital_status = f.get('marital_status')
+    # 获取emergencyContact参数
     emergencyContact = f.get('emergencyContact')
+    # 获取当前时间并转换为字符串格式作为创建日期和更新日期
     createDate = str(datetime.now())
     updateDate = str(datetime.now())
-    # db.session.add(new_patient)
-    # db.session.commit()
+    # 创建一个新的患者对象字典
     new_patient_obj = {
         "id": id,
         "username": username,
@@ -35,15 +54,37 @@ def new_patient():
         "createDate": createDate,
         "updateDate": updateDate
     }
+    # 返回新创建的患者对象的JSON格式数据
     return jsonify(new_patient_obj)
 
 
+"""
+    获取患者信息列表
+
+    Args:
+        pa_id (str): 患者ID
+
+    Returns:
+        dict: 患者信息，包含以下字段：
+            - id (str): 患者ID
+            - usrname (str): 患者姓名
+            - mobile (str): 患者手机号
+            - address (str): 患者地址
+            - job (int): 患者职业
+            - nation (str): 患者民族
+            - marital_status (int): 患者婚姻状态
+            - emergencyContact (str): 紧急联系人手机号
+            - createDate (str): 患者创建日期
+            - updateDate (str): 患者更新日期
+    """
 @bp.route('/patient_info_all', methods=['GET'])
 @bp.route('/patient_info_all/<pa_id>', methods=['GET'])
 def patient_info_all(pa_id=None):
+    # 如果 pa_id 为 None，则返回 "patient_info_all_peoples"
     if pa_id is None:
         return "patient_info_all_peoples"
     else:
+        # 创建一个名为 patient 的字典，包含患者信息
         patient = {
             'id': '10000000000000',
             'usrname': 'patienttest',
@@ -56,4 +97,6 @@ def patient_info_all(pa_id=None):
             'createDate': '2023-2-2',
             'updateDate': '2023-2-2'
         }
+        # 返回 patient 的 JSON 格式化结果
         return jsonify(patient)
+
