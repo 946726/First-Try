544753f260be39c2415f974ce3217c1a6c3f4a25
xxz <14035544+xxz496090@user.noreply.gitee.com>
2024-03-18 10:58:55 +0800

    test

--- models.py
+++ models.py
@@ -13,12 +13,11 @@ class Doctor(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
 
 
-# 用户基本信息
+#用户基本信息
 class PatientInfo(db.Model):
     __tablename__ = 'patientinfo'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    gender = db.Column(db.Integer, nullable=False)
     idNumber = db.Column(db.String(100), primary_key=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
     mobile = db.Column(db.String(100), unique=True, nullable=False)
@@ -30,8 +29,7 @@ class PatientInfo(db.Model):
     createDate = db.Column(db.DateTime, default=datetime.now)
     updateDate = db.Column(db.DateTime, default=datetime.now)
 
-
-# 疾病状况评估
+#疾病状况评估
 class sheet1B(db.Model):
     __tablename__ = 'sheet1B'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -42,8 +40,7 @@ class sheet1B(db.Model):
     recentJointSurgery = db.Column(db.Boolean, nullable=True)
     cardiovascularFamilyHistory = db.Column(db.Boolean, nullable=True)
 
-
-# 体格信息
+#体格信息
 class sheet2A(db.Model):
     __tablename__ = 'sheet2A'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -54,8 +51,7 @@ class sheet2A(db.Model):
     sbp = db.Column(db.Integer, nullable=True)
     dbp = db.Column(db.Integer, nullable=True)
 
-
-# 生化指标信息
+#生化指标信息
 class sheet2B(db.Model):
     __tablename__ = 'sheet2B'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -71,8 +67,7 @@ class sheet2B(db.Model):
     alt = db.Column(db.Double, nullable=True)
     sua = db.Column(db.Double, nullable=True)
 
-
-# 颈动脉指标信息
+#颈动脉指标信息
 class sheet2C(db.Model):
     __tablename__ = 'sheet2C'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -80,24 +75,21 @@ class sheet2C(db.Model):
     rightlmt = db.Column(db.Double, nullable=True)
     carotidAtheroscleroticPlaque = db.Column(db.Double, nullable=True)
 
-
-# ABI指标信息
+#ABI指标信息
 class sheet2D(db.Model):
     __tablename__ = 'sheet2D'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
     lestAbi = db.Column(db.Double, nullable=True)
     rightAbi = db.Column(db.Double, nullable=True)
 
-
-# 肝脏指标信息
+#肝脏指标信息
 class sheet2E(db.Model):
     __tablename__ = 'sheet2E'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
     lfap = db.Column(db.Double, nullable=True)
     lsm = db.Column(db.Double, nullable=True)
 
-
-# 体格势能表
+#体格势能表
 class sheet3(db.Model):
     __tablename__ = 'sheet3'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -108,8 +100,7 @@ class sheet3(db.Model):
     balanceTest30 = db.Column(db.String(100), nullable=True)
     balanceTest60 = db.Column(db.String(100), nullable=True)
 
-
-# 运动处方
+#运动处方
 class sheet4A(db.Model):
     __tablename__ = 'sheet4'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -118,21 +109,20 @@ class sheet4A(db.Model):
     restingHeartRate = db.Column(db.Integer, nullable=True)
     comorbidities = db.Column(db.String(100), nullable=True)
     aerobicExerciseType = db.Column(db.String(100), nullable=False)
-    # aerobicAdaptationPrescription = db.Column(db.Object, nullable=False)
-    # aerobicImprovementPrescription = db.Column(db.Object, nullable=False)
-    # aerobicStablePrescription = db.Column(db.Object, nullable=False)
+    #aerobicAdaptationPrescription = db.Column(db.Object, nullable=False)
+    #aerobicImprovementPrescription = db.Column(db.Object, nullable=False)
+    #aerobicStablePrescription = db.Column(db.Object, nullable=False)
     resistanceExerciseType = db.Column(db.String(100), nullable=True)
     coreMusclesInvolved = db.Column(db.String(100), nullable=True)
-    # resistanceAdaptationPrescription = db.Column(db.Object, nullable=True)
-    # resistanceImprovementPrescription = db.Column(db.Object, nullable=True)
-    # resistanceStablePrescription = db.Column(db.Object, nullable=True)
+    #resistanceAdaptationPrescription = db.Column(db.Object, nullable=True)
+    #resistanceImprovementPrescription = db.Column(db.Object, nullable=True)
+    #resistanceStablePrescription = db.Column(db.Object, nullable=True)
     preparationNote = db.Column(db.String(100), nullable=True)
     antidiabeticDrugNote = db.Column(db.String(100), nullable=True)
     dietNote = db.Column(db.String(100), nullable=True)
     unexpectedEventsNote = db.Column(db.String(100), nullable=True)
 
-
-# 有氧运动处方定义
+#有氧运动处方定义
 class sheet4B(db.Model):
     __tablename__ = 'sheet4B'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -147,8 +137,7 @@ class sheet4B(db.Model):
     coolingDownType = db.Column(db.String(100), nullable=False)
     coolingDownDuration = db.Column(db.Integer, nullable=False)
 
-
-# 抗阻运动处方定义
+#抗阻运动处方定义
 class sheet4C(db.Model):
     __tablename__ = 'sheet4C'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -165,7 +154,6 @@ class sheet4C(db.Model):
     coolingDownType = db.Column(db.String(100), nullable=False)
     coolingDownDuration = db.Column(db.Integer, nullable=False)
 
-
 class Admin(db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.String(100), primary_key=True, nullable=False)
@@ -177,3 +165,4 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+
