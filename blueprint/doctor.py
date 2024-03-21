463c5fc2f3af88d924cd512acd5c332441cf5e4c
xxz <14035544+xxz496090@user.noreply.gitee.com>
2024-03-21 18:52:54 +0800

    test

--- blueprint/doctor.py
+++ blueprint/doctor.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import or_
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from models import PatientInfo, sheet4B, sheet2B, sheet4A
+from models import PatientInfo, sheet4B, sheet2B, sheet4A, sheet4C
 
 bp = Blueprint('doctor', __name__, url_prefix='/')
 
@@ -108,70 +108,170 @@ def patient_info_tg(patient_id):
 
 @bp.route('/doctor/patient_info_aerobicAdaptationPrescription/<patient_id>')
 def patient_info_aa_prescription(patient_id):
-    prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
-    aa_prescription_id = prescriptions_id[0].aerobicAdaptationPrescription
-    aa_prescription = sheet4B.query.filter_by(id=aa_prescription_id).all()
-    if not aa_prescription:
-        return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
-    else:
-        aa_prescription_data = {
-            # "startTime": aa_prescription.startTime,
-            # "endTime": aa_prescription.endTime
-            "type": aa_prescription[0].type,
-            "warmUpType": aa_prescription[0].warmUpType,
-            "warmUpDuration": aa_prescription[0].warmUpDuration,
-            "heartRateLowerLimit": aa_prescription[0].heartRateLowerLimit,
-            "heartRateUpperLimit": aa_prescription[0].heartRateUpperLimit,
-            "exerciseDuration": aa_prescription[0].exerciseDuration,
-            "exerciseFrequency": aa_prescription[0].exerciseFrequency,
-            "coolingDownType": aa_prescription[0].coolingDownType,
-            "coolingDownDuration": aa_prescription[0].coolingDownDuration
-        }
-        return jsonify(aa_prescription_data)
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        aa_prescription_id = prescriptions_id[0].aerobicAdaptationPrescription
+        aa_prescription = sheet4B.query.filter_by(id=aa_prescription_id).all()
+        if not aa_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            aa_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "type": aa_prescription[0].type,
+                "warmUpType": aa_prescription[0].warmUpType,
+                "warmUpDuration": aa_prescription[0].warmUpDuration,
+                "heartRateLowerLimit": aa_prescription[0].heartRateLowerLimit,
+                "heartRateUpperLimit": aa_prescription[0].heartRateUpperLimit,
+                "exerciseDuration": aa_prescription[0].exerciseDuration,
+                "exerciseFrequency": aa_prescription[0].exerciseFrequency,
+                "coolingDownType": aa_prescription[0].coolingDownType,
+                "coolingDownDuration": aa_prescription[0].coolingDownDuration
+            }
+            return jsonify(aa_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
 
 
 @bp.route('/doctor/patient_info_aerobicImprovementPrescription/<patient_id>')
 def patient_info_ai_prescription(patient_id):
-    prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
-    ai_prescription_id = prescriptions_id[0].aerobicImprovementPrescription
-    ai_prescription = sheet4B.query.filter_by(id=ai_prescription_id).all()
-    if not ai_prescription:
-        return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
-    else:
-        ai_prescription_data = {
-            # "startTime": aa_prescription.startTime,
-            # "endTime": aa_prescription.endTime
-            "type": ai_prescription[0].type,
-            "warmUpType": ai_prescription[0].warmUpType,
-            "warmUpDuration": ai_prescription[0].warmUpDuration,
-            "heartRateLowerLimit": ai_prescription[0].heartRateLowerLimit,
-            "heartRateUpperLimit": ai_prescription[0].heartRateUpperLimit,
-            "exerciseDuration": ai_prescription[0].exerciseDuration,
-            "exerciseFrequency": ai_prescription[0].exerciseFrequency,
-            "coolingDownType": ai_prescription[0].coolingDownType,
-            "coolingDownDuration": ai_prescription[0].coolingDownDuration
-        }
-        return jsonify(ai_prescription_data)
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        ai_prescription_id = prescriptions_id[0].aerobicImprovementPrescription
+        ai_prescription = sheet4B.query.filter_by(id=ai_prescription_id).all()
+        if not ai_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            ai_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "type": ai_prescription[0].type,
+                "warmUpType": ai_prescription[0].warmUpType,
+                "warmUpDuration": ai_prescription[0].warmUpDuration,
+                "heartRateLowerLimit": ai_prescription[0].heartRateLowerLimit,
+                "heartRateUpperLimit": ai_prescription[0].heartRateUpperLimit,
+                "exerciseDuration": ai_prescription[0].exerciseDuration,
+                "exerciseFrequency": ai_prescription[0].exerciseFrequency,
+                "coolingDownType": ai_prescription[0].coolingDownType,
+                "coolingDownDuration": ai_prescription[0].coolingDownDuration
+            }
+            return jsonify(ai_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
+
 
 @bp.route('/doctor/patient_info_aerobicStablePrescription/<patient_id>')
 def patient_info_as_prescription(patient_id):
-    prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
-    as_prescription_id = prescriptions_id[0].aerobicStablePrescription
-    as_prescription = sheet4B.query.filter_by(id=as_prescription_id).all()
-    if not as_prescription:
-        return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
-    else:
-        as_prescription_data = {
-            # "startTime": aa_prescription.startTime,
-            # "endTime": aa_prescription.endTime
-            "type": as_prescription[0].type,
-            "warmUpType": as_prescription[0].warmUpType,
-            "warmUpDuration": as_prescription[0].warmUpDuration,
-            "heartRateLowerLimit": as_prescription[0].heartRateLowerLimit,
-            "heartRateUpperLimit": as_prescription[0].heartRateUpperLimit,
-            "exerciseDuration": as_prescription[0].exerciseDuration,
-            "exerciseFrequency": as_prescription[0].exerciseFrequency,
-            "coolingDownType": as_prescription[0].coolingDownType,
-            "coolingDownDuration": as_prescription[0].coolingDownDuration
-        }
-        return jsonify(as_prescription_data)
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        as_prescription_id = prescriptions_id[0].aerobicStablePrescription
+        as_prescription = sheet4B.query.filter_by(id=as_prescription_id).all()
+        if not as_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            as_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "type": as_prescription[0].type,
+                "warmUpType": as_prescription[0].warmUpType,
+                "warmUpDuration": as_prescription[0].warmUpDuration,
+                "heartRateLowerLimit": as_prescription[0].heartRateLowerLimit,
+                "heartRateUpperLimit": as_prescription[0].heartRateUpperLimit,
+                "exerciseDuration": as_prescription[0].exerciseDuration,
+                "exerciseFrequency": as_prescription[0].exerciseFrequency,
+                "coolingDownType": as_prescription[0].coolingDownType,
+                "coolingDownDuration": as_prescription[0].coolingDownDuration
+            }
+            return jsonify(as_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
+
+
+@bp.route('/doctor/patient_info_resistanceAdaptationPrescription/<patient_id>')
+def patient_info_ra_prescription(patient_id):
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        ra_prescription_id = prescriptions_id[0].resistanceAdaptationPrescription
+        ra_prescription = sheet4C.query.filter_by(id=ra_prescription_id).all()
+        if not ra_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            ra_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "resistanceExerciseType": ra_prescription[0].resistanceExerciseType,
+                "coreMusclesInvolved": ra_prescription[0].coreMusclesInvolved,
+                "warmUpType": ra_prescription[0].warmUpType,
+                "warmUpDuration": ra_prescription[0].warmUpDuration,
+                "exercises": ra_prescription[0].exercises,
+                "intensity": ra_prescription[0].intensity,
+                "number": ra_prescription[0].number,
+                "groupNumber": ra_prescription[0].groupNumber,
+                "restTime": ra_prescription[0].restTime,
+                "exerciseFrequency": ra_prescription[0].exerciseFrequency,
+                "coolingDownType": ra_prescription[0].coolingDownType,
+                "coolingDownDuration": ra_prescription[0].coolingDownDuration
+            }
+            return jsonify(ra_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
+
+
+@bp.route('/doctor/patient_info_resistanceImprovementPrescription/<patient_id>')
+def patient_info_ri_prescription(patient_id):
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        ri_prescription_id = prescriptions_id[0].resistanceImprovementPrescription
+        ri_prescription = sheet4C.query.filter_by(id=ri_prescription_id).all()
+        if not ri_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            ri_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "resistanceExerciseType": ri_prescription[0].resistanceExerciseType,
+                "coreMusclesInvolved": ri_prescription[0].coreMusclesInvolved,
+                "warmUpType": ri_prescription[0].warmUpType,
+                "warmUpDuration": ri_prescription[0].warmUpDuration,
+                "exercises": ri_prescription[0].exercises,
+                "intensity": ri_prescription[0].intensity,
+                "number": ri_prescription[0].number,
+                "groupNumber": ri_prescription[0].groupNumber,
+                "restTime": ri_prescription[0].restTime,
+                "exerciseFrequency": ri_prescription[0].exerciseFrequency,
+                "coolingDownType": ri_prescription[0].coolingDownType,
+                "coolingDownDuration": ri_prescription[0].coolingDownDuration
+            }
+            return jsonify(ri_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
+
+
+@bp.route('/doctor/patient_info_resistanceStablePrescription/<patient_id>')
+def patient_info_rs_prescription(patient_id):
+    try:
+        prescriptions_id = sheet4A.query.filter_by(id=patient_id).all()
+        rs_prescription_id = prescriptions_id[0].resistanceStablePrescription
+        rs_prescription = sheet4C.query.filter_by(id=rs_prescription_id).all()
+        if not rs_prescription:
+            return jsonify({'code': '0', 'msg': '没有找到该患者信息'})
+        else:
+            rs_prescription_data = {
+                # "startTime": aa_prescription.startTime,
+                # "endTime": aa_prescription.endTime
+                "resistanceExerciseType": rs_prescription[0].resistanceExerciseType,
+                "coreMusclesInvolved": rs_prescription[0].coreMusclesInvolved,
+                "warmUpType": rs_prescription[0].warmUpType,
+                "warmUpDuration": rs_prescription[0].warmUpDuration,
+                "exercises": rs_prescription[0].exercises,
+                "intensity": rs_prescription[0].intensity,
+                "number": rs_prescription[0].number,
+                "groupNumber": rs_prescription[0].groupNumber,
+                "restTime": rs_prescription[0].restTime,
+                "exerciseFrequency": rs_prescription[0].exerciseFrequency,
+                "coolingDownType": rs_prescription[0].coolingDownType,
+                "coolingDownDuration": rs_prescription[0].coolingDownDuration
+            }
+            return jsonify(rs_prescription_data)
+    except Exception as e:
+        return jsonify({'code': '-1', 'msg': '查询过程中出现异常: {}'.format(str(e))})
